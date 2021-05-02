9254d1e5a033a08c1f585a646028d90f943b025b
songaartan <polina.zakharenko@gmail.com>
2021-05-02 21:30:37 +0300

    доделан переход на pytest для тестов группы
    тесты успешно проходятся

--- test_add_group.py
+++ test_add_group.py
@@ -1,34 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
 import pytest
 from group import Group
 from application import Application
 
+
 @pytest.fixture
 def app(request):
-    fixture = Application
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        #initializing a fixture
-        self.app = Application
 
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="Firstgroup", header="Firstgroup", footer="Firstgroup"))
-        self.app.logout()
-
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="Firstgroup", header="Firstgroup", footer="Firstgroup"))
+    app.logout()
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
