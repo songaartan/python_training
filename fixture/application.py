65329c61cf6ae6ab1123850bb8bfbc5398669c08
songaartan <polina.zakharenko@gmail.com>
2021-05-03 11:25:36 +0300

    проверены тесты на работоспособность

--- fixture/application.py
+++ fixture/application.py
@@ -3,8 +3,8 @@ from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
-class Application:
 
+class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
