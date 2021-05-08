bf8cfb87bb1efcb5956e9c1f22c6a1ec59f83a36
songaartan <polina.zakharenko@gmail.com>
2021-05-08 20:24:27 +0300

    добавлена валидация фикстуры перед каждым тестом

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,9 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="Firstgroup", header="Firstgroup", footer="Firstgroup"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
