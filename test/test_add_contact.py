201de13181d53630557bb7d5383ba4015a4cb8de
songaartan <polina.zakharenko@gmail.com>
2021-05-25 22:40:41 +0300

    переписаны проверки в тестах

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,7 +12,8 @@ def test_add_contact(app):
                                 bday="14", bmonth="April", byear="1990",
                                 aday="22", amonth="March", ayear="2020")
     app.contact.add_new(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -11,8 +11,8 @@ def test_delete_first_contact(app):
                                 aday="22", amonth="March", ayear="2020"))
     old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    assert len(old_contacts) - 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts[0:1] = []
     assert old_contacts == new_contacts
 
